44e0edd6707dcce9cbeba1c77f9b3ebb92b7276b
IWWebSolution <iwwebsolutions@gmail.com>
2026-02-04 14:16:56 +0530

    edit 1

--- src/api/api.js
+++ src/api/api.js
@@ -1,10 +1,17 @@
 import axios from "axios";
 
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
-
 const api = axios.create({
-  baseURL: API_BASE_URL,
-  withCredentials:true,
+  baseURL: import.meta.env.VITE_API_BASE_URL,
+});
+
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem("access");
+
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+
+  return config;
 });
 
 export default api;
